3520b765242b33532e86c17252cddb2e164e9326
AliG487 <engr.ali487@gmail.com>
2026-05-29 18:11:44 +0500

    added new feature. Now user can toggle between 24-hour and 12-hour time format. This feature is implemented in the script.js file, and the corresponding UI elements are updated in index.html and styled in style.css.

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 let clockDisplay = document.getElementById("clockDisplay")
 let dateDisplay = document.getElementById("dateDisplay")
+let toglFormat = document.getElementById("t-togl")
+
+let toggle = false
+
+toglFormat.addEventListener("change", (e) => {
+  if (toglFormat.checked) {
+    toggle = true
+  } else {
+    toggle = false
+  }
+})
 
 function setTime() {
   let myTime = new Date()
 
-  let time = myTime.toLocaleTimeString()
-  let date = myTime.toDateString()
-  clockDisplay.innerText = time
-  dateDisplay.innerText = date
+  if (toggle) {
+    clockDisplay.innerText = myTime.toLocaleTimeString("en-US", {
+      hour12: false,
+    })
+    dateDisplay.innerText = myTime.toDateString()
+  } else {
+    clockDisplay.innerText = myTime.toLocaleTimeString("en-US", {
+      hour12: true,
+    })
+    dateDisplay.innerText = myTime.toDateString()
+  }
 }
 
 function runTime() {
